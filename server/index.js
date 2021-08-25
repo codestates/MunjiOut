f6a8875c17b0705c0bd4b66ac196945b05965f53
TAETAEHO <xogh9204@gmail.com>
2021-08-25 09:44:53 +0900

    add : setting

--- server/index.js
+++ server/index.js
@@ -26,16 +26,15 @@ app.use(cookieParser());
 const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 
 let server;
-if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
-
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
   const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
   const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
   const credentials = { key: privateKey, cert: certificate };
 
   server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log('https server runnning'));
+  server.listen(HTTPS_PORT, () => console.log("https server runnning"));
 } else {
-  server = app.listen(HTTPS_PORT, () => console.log('http server runnning'));
+  server = app.listen(HTTPS_PORT, () => console.log("http server runnning"));
 }
 
 module.exports = server;
