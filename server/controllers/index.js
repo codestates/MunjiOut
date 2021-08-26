294f46e351ed1a33d3d4dac99c11a63b43df9d2f
Kyungjoo Ha <429why@gmail.com>
2021-08-26 14:13:22 +0900

    modify : search controller modified

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,5 +2,6 @@ module.exports = {
     auth: require('./sign/auth'),
     signup: require('./sign/signup'),
     login: require('./sign/login'),
-    logout: require('./sign/logout')
+    logout: require('./sign/logout'),
+    search: require('./search/search')
 };
