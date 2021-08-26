34d83a621a227d5e4b5693644dd8ae2622cabf6a
TTurbo0824 <429why@gmail.com>
2021-08-26 12:07:40 +0900

    fix : typo fixed

--- server/models/location.js
+++ server/models/location.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.UserLocation, {
+      Location.hasMany(models.UserLocation, {
         foreignKey: 'locationId'
       });
     }
