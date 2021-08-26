332721989304b6e027b345616333fb28a17ad799
Kyungjoo Ha <429why@gmail.com>
2021-08-26 14:12:11 +0900

    add : new api created

---
+++ server/api/locationList.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        locationName: '서울시 강남구'
+    },
+    {
+        locationName: '서울시 강동구'
+    },
+    {
+        locationName: '서울시 강북구'
+    },
+    {
+        locationName: '서울시 강서구'
+    },
+    {
+        locationName: '서울시 관악구'
+    },
+    {
+        locationName: '서울시 광진구'
+    },
+    {
+        locationName: '서울시 구로구'
+    },
+    {
+        locationName: '서울시 금천구'
+    },
+    {
+        locationName: '서울시 노원구'
+    },
+    {
+        locationName: '서울시 도봉구'
+    },
+]
