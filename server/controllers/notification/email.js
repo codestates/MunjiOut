cf10adc4fec6e846aed8f4e2916a1253c3ef7990
Kyungjoo Ha <429why@gmail.com>
2021-08-27 14:59:53 +0900

    modify : notification controller modified

--- server/controllers/notification/email.js
+++ server/controllers/notification/email.js
@@ -1,3 +1,6 @@
+var Sequelize = require('sequelize');
+require('sequelize-values')(Sequelize);
+
 const db = require('../../models');
 
 module.exports = async (req, res) => {
@@ -8,26 +11,29 @@ module.exports = async (req, res) => {
         where: { userId: req.body.userId },
     })
     .then((data) => {
+        // console.log(Sequelize.getValues(data));
+        data = Sequelize.getValues(data);
         if (data.length > 0) {
-            // console.log(data[0].dataValues.locationId)
             data.map((el => {
-                stations.push(el.dataValues.locationId);
+                stations.push(el.locationId);
             }));
             console.log(stations);
-            // const list = [];
-            const list = db.User.findOne({
-                where: { id: 1 }
-            });
-            // stations.map((el) => {
-            //     list.push(db.Location.findOne({
-            //         where: { id: el }
-            //     }))
-            // })
-            console.log(list instanceof db.User);
-            console.log(list.id);
-            return res.status(200).send(stations);
         }
-        return res.status(404).json( { message: "no preferred locations selected" } );
     });
+
+    db.Location.findAll()
+    .then((data) => {
+        // console.log(Sequelize.getValues(data));
+        const stationList = Sequelize.getValues(data);
+        const pLocation = [];
+        stationList.filter((station) => {
+            if (stations.includes(station.id)) {
+                pLocation.push(station.location_name);
+            }
+        })
+        console.log(pLocation);
+        return res.status(200).send(pLocation);
+    })
+
     return res.status(400);
 };
