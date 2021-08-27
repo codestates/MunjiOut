baaca2b12c95dfa1d1eae658939e3988856fa376
TAETAEHO <xogh9204@gmail.com>
2021-08-28 08:31:52 +0900

    fix : to merge

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   auth: require("./sign/auth"),
   signup: require("./sign/signup"),
   login: require("./sign/login"),
@@ -14,16 +13,3 @@ module.exports = {
   unsetLocations: require("./locations/unsetLocations"),
   userLocations: require("./locations/userLocations"),
 };
-=======
-    auth: require('./sign/auth'),
-    signup: require('./sign/signup'),
-    login: require('./sign/login'),
-    logout: require('./sign/logout'),
-    search: require('./search/search'),
-    email: require('./notification/email'),
-    mainpage: require('./mainpage/mainpage'),
-    userinfo: require("./mypage/userinfo"),
-    editUserinfo: require("./mypage/editUserInfo"),
-    withdrawal: require("./mypage/withdrawal"),
-};
->>>>>>> e4067b067d023631d7de4991a7204330f157b18f
