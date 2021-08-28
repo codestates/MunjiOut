906669b569e2ddf63ea1abf715b6e9ae3e93a2fa
TTurbo0824 <429why@gmail.com>
2021-08-29 04:52:58 +0900

    modify : mainpage-controller modified

--- server/controllers/mainpage/mainpage.js
+++ server/controllers/mainpage/mainpage.js
@@ -3,12 +3,20 @@ const axios = require('axios');
 var Sequelize = require('sequelize');
 require('sequelize-values')(Sequelize);
 const db = require('../../models');
+const { isAuthorized } = require('../tokenFunctions');
 
 const apiKey = process.env.API_KEY;
-const rowNum = 5;
+const rowNum = 1;
 
 module.exports = async (req, res) => {
     // console.log('++++++++++++\n', req.body);
+    const accessTokenData = isAuthorized(req);
+    // console.log(accessTokenData);
+    if (!accessTokenData) {
+        // 로그인 상태가 아닌 경우
+        // 이 경우에는 유저 전용 메인페이지가 아니라 검색기능만 가능한 홈페이지으로 리다이렉트 되면 좋을 것 같습니다
+        return res.status(403).json({ message: 'you are not logged in' });
+    }
     const stations = [];
     
     await db.UserLocation.findAll({
@@ -18,7 +26,7 @@ module.exports = async (req, res) => {
                 attributes: ['id', 'location_name']
             }
         ],
-        where: { userId: req.body.userId },
+        where: { userId: accessTokenData.id },
     })
     .then((data) => {
         data = Sequelize.getValues(data);
@@ -28,8 +36,8 @@ module.exports = async (req, res) => {
                 stations.push(el.Location.location_name);
             }));
             // console.log(stations);
-            async function fetchStationInfo () {
-                const updated = stations.map(async (station) => {
+            const fetchStationInfo = async () => {
+                const stationInfo = stations.map(async (station) => {
                     station = station.split(' ')[1];
                     const encodedLocation = encodeURIComponent(station);
                     const url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=${encodedLocation}&dataTerm=month&pageNo=1&numOfRows=${rowNum}&returnType=json&serviceKey=${apiKey}`;
@@ -38,7 +46,15 @@ module.exports = async (req, res) => {
                         method: 'GET',
                         url: url,
                     });
-                    
+
+                    // 측정소가 점검 중일 경우 => pm10Value가 "-"으로 표기됨
+                    if (res.data.response.body.items[0].pm10Value = "-") {
+                        return {
+                            station: station,
+                            lastUpdated: res.data.response.body.items[0].dataTime,
+                            pmValue: "the station is currently under inspection."
+                        };
+                    }
                     return {
                         station: station,
                         lastUpdated: res.data.response.body.items[0].dataTime,
@@ -46,7 +62,7 @@ module.exports = async (req, res) => {
                     };
                 });
                 
-                const results = await Promise.all(updated);
+                const results = await Promise.all(stationInfo);
                 // console.log(results);
                 res.status(200).json(results);
             }
