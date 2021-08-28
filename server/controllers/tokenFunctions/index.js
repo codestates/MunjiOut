5f12fe5b49942179337ecb108ed35e06e5702b8c
Kyungjoo Ha <429why@gmail.com>
2021-08-28 13:05:54 +0900

    modify : sign controller modified

--- server/controllers/tokenFunctions/index.js
+++ server/controllers/tokenFunctions/index.js
@@ -5,17 +5,17 @@ module.exports = {
     generateAccessToken: data => {
         return sign(data, process.env.ACCESS_SECRET, { expiresIn: '1h' });
     },
-        sendAccessToken: async (res, accessToken) => {
-        await res.cookie('set-cookie', { jwt : accessToken }, { httpOnly: true, Secure: true, SameSite: "None" }).send({ message: 'ok' });
+    sendAccessToken: async (res, accessToken) => {
+      await res.cookie('set-cookie', { jwt : accessToken }, { httpOnly: true, Secure: true, SameSite: "None" }).send({ message: 'ok' });
     },
     isAuthorized: req => {
-        const accessToken = req["set-cookie"];
-
-        if (accessToken) {
-            const payload = verify(accessToken.jwt, process.env.ACCESS_SECRET);
-            return payload;
-        } else {
-            return null;
-        }
+      const accessToken = req["set-cookie"];
+      console.log(accessToken);
+      if (accessToken) {
+          const payload = verify(accessToken.jwt, process.env.ACCESS_SECRET);
+          return payload;
+      } else {
+          return null;
+      }
     },
 };
