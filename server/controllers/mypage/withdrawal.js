a852cf9b9b8d527f60147118ada8ce2627ec641a
TAETAEHO <xogh9204@gmail.com>
2021-08-28 16:03:29 +0900

    fix : mypage controllers

--- server/controllers/mypage/withdrawal.js
+++ server/controllers/mypage/withdrawal.js
@@ -3,19 +3,30 @@ const { isAuthorized } = require("../tokenFunctions");
 
 module.exports = async (req, res) => {
   try {
-    const data = isAuthorized(req);
+    // const data = isAuthorized(req);
 
-    if (!data) {
-      return res.status(404).send({ message: "회원정보를 다시 확인해주세요." });
+    // if (!data) {
+    //   return res.status(404).send({ message: "회원정보를 다시 확인해주세요." });
+    // } else {
+
+    const email = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (!email) {
+      return res
+        .status(404)
+        .send({ data: null, message: "존재하지 않는 유저입니다." });
     } else {
       await User.destroy({
         // where: { id: deleteUser.id },
-        where: { id: data.id },
+        where: { email: req.body.email },
       });
 
       res
         .status(200)
         .json({ message: "회원탈퇴가 성공적으로 완료되었습니다." });
+      // }
     }
   } catch {
     res.status(400).json({ message: "에러입니다." });
