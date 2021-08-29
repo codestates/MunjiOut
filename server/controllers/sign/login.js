fda568961d8ea2f7ad5df99891ff0c4061e8e8cc
TTurbo0824 <429why@gmail.com>
2021-08-30 08:21:23 +0900

    modify : sign-controller modified

--- server/controllers/sign/login.js
+++ server/controllers/sign/login.js
@@ -34,6 +34,7 @@ module.exports = async (req, res) => {
                 const cookieOptions = {
                     httpOnly: true,
                     sameSite: 'None',
+                    // HTTPS 통신 시에만 필요
                     // secure: true,
                 }
                   
