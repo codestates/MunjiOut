918273df13d70f3a3e586dbd65782891aedb91f3
TTurbo0824 <429why@gmail.com>
2021-08-30 08:40:07 +0900

    modify : search-controller modified

--- server/controllers/search/search.js
+++ server/controllers/search/search.js
@@ -16,31 +16,27 @@ module.exports = {
                 return res.status(400).json({ message: "please enter a search term" });
             }
            
-            if (req.query.query) {
-                console.log(req.query.query);
-                stationLocation = req.query.query
-                // console.log("station: " + stationLocation);
-                const encodedLocation = encodeURIComponent(stationLocation.split(' ')[1]);
-                const url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=${encodedLocation}&dataTerm=month&pageNo=1&numOfRows=${rowNum}&returnType=json&serviceKey=${apiKey}`;
-                fetch(url)
-                .then((res) => res.json())
-                .then((res) => {
-                    // console.log(res.response.body.items[0]);
-                    lastUpdated = res.response.body.items[0].dataTime;
-                    pmValue = res.response.body.items[0].pm10Value;
-                })
-                .then(() => {
-                    if (pmValue === "-") {
-                        res.status(200).json({ stationName: stationLocation, lastUpdated: lastUpdated, pm10_value: "현재 측정소가 점검 중입니다." })
-                    }
-                    res.status(200).json({ stationName: stationLocation, lastUpdated: lastUpdated, pm10_value: pmValue })
-                })
-                .catch(error => {
-                    throw(error);
-                });
-            } else {
-                return res.status(404).json({ message: "please enter a search term" });
-            }
+            stationLocation = req.query.query
+            // console.log("station: " + stationLocation);
+            const encodedLocation = encodeURIComponent(stationLocation.split(' ')[1]);
+            const url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=${encodedLocation}&dataTerm=month&pageNo=1&numOfRows=${rowNum}&returnType=json&serviceKey=${apiKey}`;
+            fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                // console.log(res.response.body.items[0]);
+                lastUpdated = res.response.body.items[0].dataTime;
+                pmValue = res.response.body.items[0].pm10Value;
+            })
+            .then(() => {
+                if (pmValue === "-") {
+                    res.status(200).json({ stationName: stationLocation, lastUpdated: lastUpdated, pm10_value: "현재 측정소가 점검 중입니다." })
+                }
+                res.status(200).json({ stationName: stationLocation, lastUpdated: lastUpdated, pm10_value: pmValue })
+            })
+            .catch(error => {
+                throw(error);
+            });
+            
         } else {
             return res.status(400);
         }
