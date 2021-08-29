99666bca0da88451ba8ddc4ecd749bb14b19cb23
TAETAEHO <xogh9204@gmail.com>
2021-08-29 18:17:06 +0900

    fix : location controller

--- server/controllers/locations/userLocations.js
+++ server/controllers/locations/userLocations.js
@@ -5,37 +5,48 @@ const { isAuthorized } = require("../tokenFunctions");
 
 module.exports = async (req, res) => {
   try {
-    const data = isAuthorized(req);
+    // const data = isAuthorized(req);
 
-    if (!data) {
+    // if (!data) {
+    //   return res
+    //     .status(404)
+    //     .send({ data: null, message: "존재하지 않는 유저입니다." });
+    // } else {
+
+    const email = await User.findOne({
+      where: { email: req.body.email },
+    });
+    console.log(req.body);
+
+    if (!email) {
       return res
         .status(404)
         .send({ data: null, message: "존재하지 않는 유저입니다." });
     } else {
-      delete data.password;
-
       const result = await User.findAll({
         where: {
-          id: data.id,
+          email: req.body.email,
         },
         include: [
           {
             model: UserLocation,
             required: false,
-            // attributes: ["userId", "locationId"],
-          },
-          {
-            model: Location,
-            required: false,
-            // attributes: ["location_name"],
+            attributes: ["userId", "locationId"],
+            include: [
+              {
+                model: Location,
+                required: false,
+                attributes: ["location_name"],
+              },
+            ],
           },
         ],
-        raw: true,
       });
 
       res
         .status(200)
         .json({ data: result, message: "해당 정보를 불러왔습니다." });
+      // }
     }
   } catch {
     res.status(400).json({ message: "에러입니다." });
