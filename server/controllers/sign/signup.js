0d866f030174cdc13f4c4f96eff1743839a48d32
TTurbo0824 <429why@gmail.com>
2021-08-30 19:07:58 +0900

    modify : sign-controller modified

--- server/controllers/sign/signup.js
+++ server/controllers/sign/signup.js
@@ -1,50 +1,52 @@
-const { isAuthorized } = require("../tokenFunctions");
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
-
+const { isAuthorized } = require('../tokenFunctions');
 const crypto = require('crypto');
 const db = require('../../models');
 
-
 module.exports = async (req, res) => {
-  // console.log('++++++++++++\n', req.body)
-  const accessTokenData = isAuthorized(req);
+    // console.log('++++++++++++\n', req.body)
+    const accessTokenData = isAuthorized(req);
 
-  // 이미 로그인 중인 상태에서 회원가입을 시도하는 경우
-  if (accessTokenData) {
-    return res.status(403).json({ message: "you are already a user" });
-  }
+    // 이미 로그인 중인 상태에서 회원가입을 시도하는 경우
+    if (accessTokenData) {
+        return res.status(403).json({ message: 'you are already a user' });
+    }
 
-  if (
-    !req.body.username ||
-    !req.body.email ||
-    !req.body.password ||
-    !req.body.mobile ||
-    !req.body.address
-  ) {
-    return res
-      .status(422)
-      .json({ message: "insufficient parameters supplied" });
-  }
+    if (
+        !req.body.username ||
+        !req.body.email ||
+        !req.body.password ||
+        !req.body.mobile ||
+        !req.body.address
+    ) {
+        return res.status(422).json({ message: 'insufficient parameters supplied' });
+    }
 
+    let conflictMessage;
 
-    const isNewUser = await db.User.findOne({
+    const isEmailConflict = await db.User.findAll({
         where: {
-            [Op.or]: [{
-                username: req.body.username
-            }],
-            [Op.or]: [{
-                email: req.body.email
-            }],
-            [Op.or]: [{
-                mobile: req.body.mobile
-            }]
+            email: req.body.email
         }
     });
-    // console.log(isNewUser);
 
-    if (isNewUser) {
-        return res.status(409).json({ message: 'conflicting user info exists' });
+    const isMobileConflict = await db.User.findAll({
+        where: {
+            mobile: req.body.mobile
+        }
+    });
+
+    // console.log(isEmailConflict.length + "\n"+ isMobileConflict.length);
+    
+    if (isEmailConflict.length > 0 || isMobileConflict.length > 0) {
+        if (isEmailConflict.length > 0 && isMobileConflict.length > 0) {
+            conflictMessage = "conflict: email & mobile";
+        } else if (isEmailConflict.length > 0 && isMobileConflict.length == 0) {
+            conflictMessage = "conflict: email";
+        } else if (isEmailConflict.length == 0 && isMobileConflict.length > 0) {
+            conflictMessage = "conflict: mobile";
+        } 
+        // console.log(conflictMessage)
+        return res.status(409).json({ message: conflictMessage });
     }
 
     const salt = crypto.randomBytes(64).toString('hex');
